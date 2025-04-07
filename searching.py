0885038bcbbe93b5bf8d243897905a63c546ab4f
tesarovaveronika <269127@vut.cz>
2025-04-07 09:38:45 +0200

    neconoveho

--- searching.py
+++ searching.py
@@ -21,12 +21,39 @@ def read_data(file_name, field):
         seq = json.load(json_file)
     return seq[field]
 
+def linear_search(seq, number):
+    slovnik = {}
+    positions= []
+    for idx, n in enumerate(seq):
+        if n == number:
+            positions.append(idx)
+    slovnik["positions"] = positions
+    count = len(positions)
+    slovnik["count"] = count
+    return slovnik
+
+def pattern_search(sekvence_dna, vzor):
+    idx = 0
+    idxs_of_vzor= set()
+    while idx < len(sekvence_dna):
+        n= len(vzor)
+        if sekvence_dna[idx:idx+n] == vzor:
+            idxs_of_vzor.add((idx,idx+n-1))
+        idx +=1
+    return idxs_of_vzor
+
 
 def main():
     file_name = "sequential.json"
 
     #read data
     seq = read_data(file_name, field="unordered_numbers")
+    sekvence_dna = read_data(file_name, field="dna_sequence")
+    idxs_of_vzor = pattern_search(sekvence_dna, vzor="ATA")
+    print(idxs_of_vzor)
     print(seq)
+    slovnik = linear_search(seq,number=0)
+    print(slovnik)
+
 if __name__ == '__main__':
     main()
